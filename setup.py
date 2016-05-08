66d8cd7496d815d2eb64c4d7b3594ae77e963dd3
Greg Brockman <gdb@gregbrockman.com>
2016-05-07 22:23:14 -0700

    DRY up the build steps

--- setup.py
+++ setup.py
@@ -6,36 +6,29 @@ from distutils import sysconfig
 from distutils.command.build import build as DistutilsBuild
 from setuptools import setup, Extension
 
-def build_osx():
+def build_common(dynamic_library_extension):
     python_include = sysconfig.get_python_inc()
-    python_library = os.path.join(sysconfig.get_config_var('LIBPL'), 'libpython{}.dylib'.format(sysconfig.get_python_version()))
+    python_library = os.path.join(sysconfig.get_config_var('LIBPL'), 'libpython{}.{}'.format(sysconfig.get_python_version(), dynamic_library_extension))
     assert os.path.exists(python_library), "Incorrectly inferred your Python dynamic library would be at {}. This indicates a bug in doom-py and should be reported.".format(python_library)
 
     subprocess.check_call(['cmake', '-DCMAKE_BUILD_TYPE=Release', '-DBUILD_PYTHON=ON', '-DBUILD_JAVA=OFF', '-DPYTHON_INCLUDE_DIR={}'.format(python_include), '-DPYTHON_LIBRARY={}'.format(python_library)], cwd='doom_py')
     subprocess.check_call(['make'], cwd='doom_py')
     subprocess.check_call(['rm', '-f', 'vizdoom.so'], cwd='doom_py')
     subprocess.check_call(['ln', '-s', 'bin/python/vizdoom.so', 'vizdoom.so'], cwd='doom_py')
+
+def build_osx():
+    build_common('dylib')
 
     # Symlink to the correct vizdoom binary
     subprocess.check_call(['rm', '-f', 'bin/vizdoom'], cwd='doom_py')
     subprocess.check_call(['ln', '-s', 'vizdoom.app/Contents/MacOS/vizdoom', 'bin/vizdoom'], cwd='doom_py')
 
 def build_linux():
-    python_include = sysconfig.get_python_inc()
-    python_library = os.path.join(sysconfig.get_config_var('LIBPL'), 'libpython{}.so'.format(sysconfig.get_python_version()))
-    assert os.path.exists(python_library), "Incorrectly inferred your Python dynamic library would be at {}. This indicates a bug in doom-py and should be reported.".format(python_library)
-
-    subprocess.check_call(['cmake', '-DCMAKE_BUILD_TYPE=Release', '-DBUILD_PYTHON=ON', '-DBUILD_JAVA=OFF', '-DPYTHON_INCLUDE_DIR={}'.format(python_include), '-DPYTHON_LIBRARY={}'.format(python_library)], cwd='doom_py')
-    subprocess.check_call(['make'], cwd='doom_py')
-    subprocess.check_call(['rm', '-f', 'vizdoom.so'], cwd='doom_py')
-    subprocess.check_call(['ln', '-s', 'bin/python/vizdoom.so', 'vizdoom.so'], cwd='doom_py')
+    build_common('so')
 
 def build_windows():
     # THIS IS UNTESTED
-    subprocess.check_call(['cmake', '-DCMAKE_BUILD_TYPE=Release', '-DBUILD_PYTHON=ON', '-DBUILD_JAVA=OFF', '-DPYTHON_INCLUDE_DIR={}'.format(sysconfig.get_python_inc())], cwd='doom_py')
-    subprocess.check_call(['make'], cwd='doom_py')
-    subprocess.check_call(['rm', '-f', 'vizdoom.so'], cwd='doom_py')
-    subprocess.check_call(['ln', '-s', 'bin/python/vizdoom.so', 'vizdoom.so'], cwd='doom_py')
+    build_common('dll')
 
 if sys.platform.startswith("darwin"):
     platname = "osx"
